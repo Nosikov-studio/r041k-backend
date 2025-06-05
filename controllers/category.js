916edc41072a937feb8f4e3a57dca4cf4f68de25
Nos <zvezdazanatih@gmail.com>
2025-06-05 14:25:49 +0700

    29

--- controllers/category.js
+++ controllers/category.js
@@ -48,9 +48,20 @@ module.exports.create = async function(req, res) {
     }
 }
 
-module.exports.update = function(req, res) {
-        try {
-
+module.exports.update =async function(req, res) {
+    const updated ={
+        name: req.body.name
+    }
+    if (req.file) {
+        updated.imageSrc = req.file.path
+    }
+    try {
+        const category = await Category.findByIdAndUpdate(
+            {_id: req.params.id},
+            {$set: updated},
+            {new: true}
+        )
+        res.status(200).json(category)
     } catch (e) {
         errorHandler(res, e)
     }
