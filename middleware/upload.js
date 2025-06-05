6246189728fa9a0d8536c26ad48e14e6f2a25508
Nos <zvezdazanatih@gmail.com>
2025-06-05 11:40:24 +0700

    25

--- middleware/upload.js
+++ middleware/upload.js
@@ -13,17 +13,20 @@ const storage = multer.diskStorage({
     }
 })
 
+const types = ['image/png', 'image/jpg', 'image/jpeg']
+
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
-        cb(null, true)
+//    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg'|| file.mimetype === 'image/jpeg') {
+if (types.includes(file.mimetype)) {
+         cb(null, true)
     } else {
         cb(null, false)
     }
 }
 
-const limits = {
-    fileSize: 1024 * 1024 * 5
-}
+// const limits = {
+//     fileSize: 1024 * 1024 * 5
+// }
 
 module.exports = multer({
     storage: storage,
